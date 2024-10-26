5b0369c44b690d13e742a17197ec30c8ab3f3842
Faiz-Ahmed10 <faizahmedhyd7@gmail.com>
2024-10-27 05:15:50 +0530

    Top 50 Books Recommendation Added

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import pickle
+
+popular_df = pickle.load(open('popular.pkl', 'rb'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    rating = list(popular_df['mean-ratings'].values)
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author = list(popular_df['Book-Author'].values),
+                           image = list(popular_df['Image-URL-M'].values),
+                           votes = list(popular_df['num-ratings'].values),
+                           rounded_ratings = [round(r) for r in rating]
+                           )
 
 if __name__ == '__main__':
     app.run(debug = True)
